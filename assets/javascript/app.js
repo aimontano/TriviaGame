5c14f0e3da4e1d4a3f3bb00ace7a321827835d3f
aimontano <aimorales.montano@gmail.com>
2018-08-22 11:53:22 -0400

    Added template folder and js request for questions works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
+	let questions = [];
+	let answerChoices = [];
 
-	$('#start').click(function(){
-		$.ajax({
-			url: '../templates/questions.html',
-			method: 'GET'
-		}).then(function(response){
-			$('#content').html(response);
+	let queryUrl = "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple";
+
+	let xhr = $.get(queryUrl, function(res){
+		questions = res;
+	});
+
+	$('#start').click(function(evt){
+		console.log(questions);
+		$('#content').load('../templates/questions.html', function(){
+			$('#question').html(questions.results[0].question);
+			$('#choice1').html(questions.results[0].incorrect_answers[0]);
+			$('#choice2').html(questions.results[0].incorrect_answers[1]);
+			$('#choice3').html(questions.results[0].incorrect_answers[2]);
+			$('#choice4').html(questions.results[0].correct_answer);
+			console.log(questions.results[0].correct_answer);
 		});
 	});
-	
 });
